b6c849842fc57bb728ab17e068f94bf0e65d97f3
Shaik Nagoor Basha <162982084+sbasha12345@users.noreply.github.com>
2024-03-11 16:20:39 +0530

    Add files via upload

---
+++ file_1.py
@@ -0,0 +1,85 @@
+#print ("hello world")
+
+#import this
+#print(this)
+
+import antigravity
+
+
+WHAT KIND OF LANGUAGE IS PYTHON
+-->object oriented programming language
+-->dynamically typed language
+-->scripting language
+-->interpreted language
+-->compiled language
+-->funtional programming language
+
+BARGAINED CHARACTER
+
+
+print has 5 parameters
+# object -->the value to be printed is object
+# sep--> seperator
+# Eg
+# a = 8
+# b = 9
+# c = 89
+1
+# print(a, b,c, sep='$')
+# end --> which is used to add the string at the end of the value,
+# # It will not keep the command to stay in same line
+# Eg:
+# print(str(a), b, end='%%')
+# print(str(c))
+
+FLAVOURS OF PYTHON:-
+
+>Cpython - Written in C, Most common Implementation of python.
+>Jython - Java implementation of Python.
+>IronPython - implementation of the Python programming language which is tightly integrated with .NET
+>Brython - Browser Python, runs in browsers
+>A RubyPython - Bridge between Python and Ruby Interpreters
+>MicroPython - Runs in Micro Controllers
+
+
+
+#--->print(object,sep,end,file,flush)
+print has 5 parameters
+# object -->the value to be printed is object
+# sep--> seperator
+# Eg
+# a = 8
+# b = 9
+# c = 89
+1
+# print(a, b,c, sep='$')
+# end --> which is used to add the string at the end of the value,
+# # It will not keep the command to stay in same line
+# Eg:
+# print(str(a), b, end='%%')
+# print(str(c))
+
+
+
+  #print("hello")
+
+
+print("hello world")
+print("hello world")
+print("hello world")
+
+1).Commenting is used for making documentation in a python file.
+2).When the line is commented , that line will not be considered as a part of the code.
+3).When the program is exucuted, commented line will not be in the part of execution.
+
+------> NOTE
+Single line commenting can be done using #
+'''
+#print("hello world")
+#print("hello world")
+#import this
+#print(this)
+
+
+# import is a keybord which is used to communicated between the python files.
+import antigravity
